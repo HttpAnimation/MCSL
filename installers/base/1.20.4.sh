12adf36a1b0c1e502a36acf412ae93c6ff275292
HttpAnimation <sahwndouglasyt@gmail.com>
2024-03-30 16:30:16 -0700

    format

--- installers/base/1.20.4.sh
+++ installers/base/1.20.4.sh
@@ -3,20 +3,24 @@
 # Variables
 minecraft_version="1.20.4"
 download_url="https://piston-data.mojang.com/v1/objects/8dd1a28015f51b1803213892b50b7b4fc76e594d/server.jar"
-config="https://raw.githubusercontent.com/HttpAnimation/MCSL/main/installers/base/config.conf"
+config_url="https://raw.githubusercontent.com/HttpAnimation/MCSL/main/installers/base/config.conf"
 downloaded_file="baseServer${minecraft_version}.jar"
+config_file="config.conf"
 target_directory="Base${minecraft_version}"
 
 # Download Minecraft server
 echo "Downloading Minecraft ${minecraft_version}..."
 curl -o "$downloaded_file" "$download_url"
+echo "Done downloading"
+
+# Download config file
 echo "Downloading the config file ${minecraft_version}..."
-curl -o "$config"
+curl -o "$config_file" "$config_url"
 echo "Done downloading"
 
 # Rename the downloaded server
 echo "Renaming the downloaded server..."
-mv "$downloaded_file" "${target_directory}.jar"
+mv "$downloaded_file" "${target_directory}/baseServer${minecraft_version}.jar"
 echo "Done renaming"
 
 # Create a new directory
@@ -25,9 +29,9 @@ mkdir "$target_directory"
 echo "Done creating directory"
 
 # Move files to new directory
-echo "Moving fils to new directory..."
-mv "${target_directory}.jar" "$target_directory/baseServer${minecraft_version}.jar"
-mv ""
+echo "Moving files to new directory..."
+mv "$config_file" "$target_directory/$config_file"
+
 echo "Done moving"
 
 echo "Done :3"
