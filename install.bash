0fa662bccd777472ba9090b9ad79b3fab606e974
HttpAnimation <sahwndouglasyt@gmail.com>
2024-03-30 16:33:15 -0700

    https://github.com/HttpAnimation/MCSL/raw/main/MCSL

--- install.bash
+++ install.bash
@@ -1,3 +1,4 @@
 #!/bin/bash
 curl -o https://raw.githubusercontent.com/HttpAnimation/MCSL/main/MCSL.py
 curl -o https://raw.githubusercontent.com/HttpAnimation/MCSL/main/config.conf
+curl -o https://github.com/HttpAnimation/MCSL/raw/main/MCSL
