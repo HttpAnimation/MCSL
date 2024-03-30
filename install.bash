624ac6cb7b9f52e54f08c3e75a1cd06822efd6dc
HttpAnimation <sahwndouglasyt@gmail.com>
2024-03-30 16:33:54 -0700

    downloads with names

--- install.bash
+++ install.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -o https://raw.githubusercontent.com/HttpAnimation/MCSL/main/MCSL.py
-curl -o https://raw.githubusercontent.com/HttpAnimation/MCSL/main/config.conf
-curl -o https://github.com/HttpAnimation/MCSL/raw/main/MCSL
-curl -o https://raw.githubusercontent.com/HttpAnimation/MCSL/main/LICENSE
+curl -o MCSL.py https://raw.githubusercontent.com/HttpAnimation/MCSL/main/MCSL.py
+curl -o config.conf https://raw.githubusercontent.com/HttpAnimation/MCSL/main/config.conf
+curl -o MCSL https://github.com/HttpAnimation/MCSL/raw/main/MCSL
+curl -o LICENSE https://raw.githubusercontent.com/HttpAnimation/MCSL/main/LICENSE
