8a03b8e29ca3340925545769c4e3eac2f0cf5bf4
HttpAnimation <sahwndouglasyt@gmail.com>
2024-03-30 16:20:23 -0700

    varibles

--- installers/base/1.20.4.sh
+++ installers/base/1.20.4.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
-echo "Downloading Minecraft 1.20.4..."
-curl -o baseServer1.20.4.jar https://piston-data.mojang.com/v1/objects/8dd1a28015f51b1803213892b50b7b4fc76e594d/server.jar
+# Variables
+minecraft_version="1.20.4"
+download_url="https://piston-data.mojang.com/v1/objects/8dd1a28015f51b1803213892b50b7b4fc76e594d/server.jar"
+downloaded_file="baseServer${minecraft_version}.jar"
+target_directory="Base${minecraft_version}"
+
+# Download Minecraft server
+echo "Downloading Minecraft ${minecraft_version}..."
+curl -o "$downloaded_file" "$download_url"
 echo "Done downloading"
 
+# Rename the downloaded file
 echo "Renaming the downloaded file..."
-mv baseServer1.20.4.jar Base1.20.4.jar
+mv "$downloaded_file" "${target_directory}.jar"
 echo "Done renaming"
 
-echo "Making new directory..."
-mkdir Base1.20.4
-echo "Done making directory"
+# Create a new directory
+echo "Creating new directory..."
+mkdir "$target_directory"
+echo "Done creating directory"
 
+# Move server to new directory
 echo "Moving server to new directory..."
-mv Base1.20.4.jar Base1.20.4/baseServer1.20.4.jar
+mv "${target_directory}.jar" "$target_directory/baseServer${minecraft_version}.jar"
 echo "Done moving"
 
 echo "Done :3"
