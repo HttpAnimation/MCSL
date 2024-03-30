5788ed71244f113f94ec44b20d3acc8e3d4ab6f7
HttpAnimation <sahwndouglasyt@gmail.com>
2024-03-30 14:19:58 -0700

    prints the servers if nothing is in

--- MCSL.py
+++ MCSL.py
@@ -15,10 +15,21 @@ def launch_server(server_name, config_path):
     print("Starting server...")
     subprocess.run(config[0]['LaunchCommand'], shell=True)
 
+def print_server_names():
+    config = read_config("config.json")
+    print("Available servers:")
+    for server_name in config[0]:
+        if server_name != "ServerNameHere":
+            print(server_name)
+
 def main():
     parser = argparse.ArgumentParser(description="Minecraft Server Launcher")
-    parser.add_argument("server_name", type=str, help="Name of the server to launch")
+    parser.add_argument("server_name", nargs='?', type=str, help="Name of the server to launch")
     args = parser.parse_args()
+
+    if args.server_name is None:
+        print_server_names()
+        return
 
     config = read_config("config.json")
     if args.server_name not in config[0]:
