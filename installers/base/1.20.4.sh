a668dc24136081a4d822eaf711568c685f26ce76
HttpAnimation <sahwndouglasyt@gmail.com>
2024-03-30 16:19:03 -0700

    formated

--- installers/base/1.20.4.sh
+++ installers/base/1.20.4.sh
@@ -1,13 +1,19 @@
-echo "Download minecraft 1.20.4."
-curl -o https://piston-data.mojang.com/v1/objects/8dd1a28015f51b1803213892b50b7b4fc76e594d/server.jar
+#!/bin/bash
+
+echo "Downloading Minecraft 1.20.4..."
+curl -o baseServer1.20.4.jar https://piston-data.mojang.com/v1/objects/8dd1a28015f51b1803213892b50b7b4fc76e594d/server.jar
 echo "Done downloading"
-echo "Ranaming the downloaded file"
-mv server.jar baseServer1.20.4.jar
+
+echo "Renaming the downloaded file..."
+mv baseServer1.20.4.jar Base1.20.4.jar
 echo "Done renaming"
-echo "Making new dir"
+
+echo "Making new directory..."
 mkdir Base1.20.4
-echp "Done making dir"
-echo "Moving server to new dir"
-cp baseServer1.20.4.jar Base1.20.4/baseServer1.20.4.jar
-echo "Down moving"
+echo "Done making directory"
+
+echo "Moving server to new directory..."
+mv Base1.20.4.jar Base1.20.4/baseServer1.20.4.jar
+echo "Done moving"
+
 echo "Done :3"
