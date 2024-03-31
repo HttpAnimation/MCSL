1241ecca8d6e15d173b282fb7f2037b7cce1cb13
HttpAnimation <sahwndouglasyt@gmail.com>
2024-03-30 18:47:05 -0700

    Fabric-1.20.4

--- installers/fabric/1.20.4.sh
+++ installers/fabric/1.20.4.sh
@@ -3,3 +3,5 @@ cd Downloading
 
 wget https://meta.fabricmc.net/v2/versions/loader/1.20.4/0.15.7/1.0.0/server/jar
 wget https://raw.githubusercontent.com/HttpAnimation/MCSL/main/installers/fabric/config.conf
+
+mkdir Fabric-1.20.4
