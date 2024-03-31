c291f80e40a0a9f419b92eb7193400f11f10f0ec
HttpAnimation <sahwndouglasyt@gmail.com>
2024-03-30 18:51:33 -0700

    varibles

--- installers/fabric/1.20.4.sh
+++ installers/fabric/1.20.4.sh
@@ -1,12 +1,13 @@
 mkdir Downloading
 cd Downloading 
 
-wget https://meta.fabricmc.net/v2/versions/loader/1.20.4/0.15.7/1.0.0/server/jar
-wget https://raw.githubusercontent.com/HttpAnimation/MCSL/main/installers/fabric/config.conf
+MinecraftJarFile = https://meta.fabricmc.net/v2/versions/loader/1.20.4/0.15.7/1.0.0/server/jar
+ConfFile = https://raw.githubusercontent.com/HttpAnimation/MCSL/main/installers/fabric/config.conf
+NewFolder = Fabric-1.20.4
 
-mkdir Fabric-1.20.4
+mkdir $NewFolder
 
-mv fabric-server-mc.1.20.4-loader.0.15.7-launcher.1.0.0.jar Fabric-1.20.4
-mv config.conf Fabric-1.20.4
-mv Fabric-1.20.4 ../../
+mv fabric-server-mc.1.20.4-loader.0.15.7-launcher.1.0.0.jar $NewFolder
+mv $ConfFile $NewFolder
+mv $NewFolder ../../
 rm -rf Downloading
