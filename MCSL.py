b90c5f516d862adebaff1a03df88f6ff83acc25b
HttpAnimation <sahwndouglasyt@gmail.com>
2024-03-31 20:59:33 -0700

    server index

--- MCSL.py
+++ MCSL.py
@@ -6,22 +6,26 @@ app = Flask(__name__)
 def read_config_file(filename):
     config = {}
     with open(filename, 'r') as file:
-        lines = file.readlines()
-        for line in lines:
-            if '=' in line:
-                key, value = line.strip().split('=')
-                config[key.strip()] = value.strip()
+        server_data = {}
+        for line in file:
+            line = line.strip()
+            if line.startswith('[') and line.endswith(']'):
+                server_name = line[1:-1]
+                config[server_name] = server_data
+                server_data = {}
+            elif '=' in line:
+                key, value = line.split('=', 1)
+                server_data[key.strip()] = value.strip()
     return config
 
 @app.route('/')
 def index():
     config = read_config_file('config.conf')
-    server_name = list(config.keys())[0]
-    return render_template('index.html', server_name=server_name, config=config)
+    return render_template('index.html', servers=config)
 
 @app.route('/launch', methods=['POST'])
 def launch_server():
-    launch_command = request.form['launch_command']
+    launch_command = request.form['server']
     process = subprocess.Popen(launch_command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
     output = stdout.decode() + '\n' + stderr.decode()
