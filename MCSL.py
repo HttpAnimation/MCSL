68497af055ae94330307248d2da7c334e67aa480
HttpAnimation <sahwndouglasyt@gmail.com>
2024-04-01 08:55:30 -0700

    rewrite

---
+++ MCSL.py
@@ -0,0 +1,24 @@
+import configparser
+import subprocess
+
+# Read config file
+config = configparser.ConfigParser()
+config.read('config.conf')
+
+# Display server information
+for server in config.sections():
+    print(f"{server} V:{config[server]['MinecraftVersion']} | Modded = {config[server]['Modded']} | Description: {config[server]['Description']}")
+
+# Prompt user for server selection
+selected_server = input("Enter the name of the server to launch: ")
+
+# Check if selected server exists in config
+if selected_server not in config.sections():
+    print("Invalid server name.")
+    exit()
+
+# Get launch command for selected server
+launch_command = config[selected_server]['LaunchCommand']
+
+# Execute launch command
+subprocess.run(launch_command, shell=True)
